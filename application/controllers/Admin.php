a1077e8412f93c4398117ec766c930f6a0d88d74
Filipe Ferreira <filipeferreira@MacBook-Pro-de-Filipe.local>
2018-12-04 10:16:26 +0000

    add functions

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -18,17 +18,61 @@ class Admin extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+
+	/*
+		CREATE TABLE IF NOT EXISTS `ci_sessions` (
+        	`id` varchar(128) NOT NULL,
+        	`ip_address` varchar(45) NOT NULL,
+        	`timestamp` int(10) unsigned DEFAULT 0 NOT NULL,
+        	`data` blob NOT NULL,
+        	KEY `ci_sessions_timestamp` (`timestamp`)
+		);
+	*/
+
+	/* 
+		Created: 4/12/2018
+		Create a middleware of session
+	*/
+	public function __construct(){
+		parent::__construct();
+		$this->load->library('session');
+		
+		//Verify is user is logged
+		if(current_url() != base_url('admin/login'))
+			$this->verify_login();
+	}
+
+	/* 
+		Created: 3/12/2018
+		Load the interface admin
+	*/
 	public function index()
 	{
         $this->load->view('admin/partials/header.php');
         $this->load->view('admin/index.html');
         $this->load->view('admin/partials/footer.php');
 	}
-	
+
+	/* 
+		Created: 3/12/2018
+		Load the login page
+	*/
 	public function login(){
+		//password_hash('teste', PASSWORD_BCRYPT);
 		$this->load->view('admin/partials/header.php');
         $this->load->view('admin/login.html');
         $this->load->view('admin/partials/footer.php');
+	}
+
+	/* 
+		Created: 4/12/2018
+		Verify if user is logged
+	*/
+	public function verify_login(){
+		if(!$this->session->has_userdata('logged'))
+			redirect('admin/login');
+
 	}
 
     
