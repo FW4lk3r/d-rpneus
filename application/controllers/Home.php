7a12bc96308fb8aa2d69719004da178f676c2bc1
Filipe Ferreira <filipeferreira@MacBook-Pro-de-Filipe.local>
2018-12-11 15:06:57 +0000

    add view frontend

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,7 +20,9 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-        
+        $this->load->view('header');
+        $this->load->view('index');
+        $this->load->view('footer');
     }
     
     
