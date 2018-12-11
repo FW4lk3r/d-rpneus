804b472b55114650f93d2240e5c3bf17981a879d
Filipe Ferreira <filipeferreira@MacBook-Pro-de-Filipe.local>
2018-12-11 15:07:16 +0000

    add header frontend

---
+++ application/views/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style-frontend.css') ?>">
+        <META charset="utf-8">
+        <title> D&R PNEUS </title>
+        </head>
+<body>
+
+  <section id="paginainicial">
+        <header>
+                <div class="container before_nav">
+                            <div class="row">
+                                <div class="col-md-4">
+                                        <img src="<?= base_url('assets/img/logo.png');?>" alt="logo" class="logo"/>
+                                </div>
+                                <div class="col-md-4 icon">
+                                                <span class="glyphicon glyphicon-earphone telefone"></span>
+                                    </div>
+                                 <div class="col-md-4 ajuda">
+                                        <div class="help">
+                                            Precisa de ajuda?
+                                        </div>
+                                        <div class="n_tel">
+                                            9*********
+                                        </div>
+                                </div>
+                            </div>
+                    </div>
+                    <nav class="navbar navbar-inverse navbar-fixed-top">
+                            <div class="container-fluid">
+                              <div class="navbar-header">
+                                <!--<a class="navbar-brand" href="#">D&R PNEUS</a>-->
+                                <button class ="navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                   </button>
+                              </div>
+                             
+                
+                              <div class="collapse navbar-collapse navHeaderCollapse" id="centro">
+                              <ul class="nav navbar-nav menu">
+                                <li class="active"><a href="#paginainicial">Página Inicial</a></li>
+                                <li><a href="#servicos">Serviços</a></li>
+                                <!--<li role="presentation" class="dropdown">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                                          Pneus <span class="caret"></span>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                          <li>Pneus Carros</li>
+                                          <li>Pneus Motos</li>
+                                        </ul>
+                                      </li>-->
+                                <li><a href="#pneus" class="palavra">Pneus</a></li>
+                                <li><a href="#empresa" class="palavra">Jantes</a></li>
+                                <li><a href="#contactos">Contactos</a></li>
+                              </ul>
+                              </div>
+                            </div>
+                          </nav>
+            
+            </header>
