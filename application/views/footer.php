f2472e7c970d3a163c02cdff20feae0e1849d503
Filipe Ferreira <filipeferreira@MacBook-Pro-de-Filipe.local>
2018-12-11 15:07:09 +0000

    add footer frontend

---
+++ application/views/footer.php
@@ -0,0 +1,19 @@
+
+  
+  <footer>
+        <div class="container">
+            <div class="row col_footer">
+                <div class="col-md-12">
+                        © 2018 Copyright:
+                            <a href="#" class="site_link"> nomesite.com</a>
+                </div>
+            </div>
+        </div>
+              
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>      
+    </footer>
+
+</body>
+
+</html>
