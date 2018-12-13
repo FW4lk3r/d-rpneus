a5762347cbc3931c53ae0abca28153ea9ded1c80
Filipe Ferreira <filipeferreira@MacBook-Pro-de-Filipe.local>
2018-12-13 10:17:25 +0000

    fix some url

--- application/views/admin/partials/header_intern.php
+++ application/views/admin/partials/header_intern.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Dashboard</title>
+  <title>D&R Pneus</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -62,7 +62,7 @@
 
   <header class="main-header">
     <!-- Logo -->
-    <a href="index2.html" class="logo">
+    <a href="<?= base_url('admin');?>" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>D&R</b></span>
       <!-- logo for regular state and mobile devices -->
