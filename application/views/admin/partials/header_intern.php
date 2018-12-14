7032847b4704ab132b854631ec6999f92d8d7d3a
Filipe Ferreira <filipeferreira@MacBook-Pro-de-Filipe.local>
2018-12-14 17:23:00 +0000

    fix image profil

--- application/views/admin/partials/header_intern.php
+++ application/views/admin/partials/header_intern.php
@@ -80,17 +80,17 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="<?php echo base_url('assets/dist/img/user2-160x160.jpg');?>" class="user-image" alt="User Image">
+              <img src="<?php echo base_url('assets/uploads/'.$_SESSION['profil-img']);?>" style="object-fit: cover;"class="user-image" alt="User Image">
               <span class="hidden-xs"> <?php echo $_SESSION['nome']?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="<?php echo base_url('assets/dist/img/user2-160x160.jpg');?>" class="img-circle" alt="User Image">
+                <img src="<?php echo base_url('assets/uploads/'. $_SESSION['profil-img']);?>" style="object-fit: cover;"class="img-circle" alt="User Image">
 
                 <p>
                   <?php echo $_SESSION['nome']?>
-                  <small><?php echo $_SESSION['created_at']; ?></small>
+                  <small>Membro desde <?php echo date('d-m-Y',strtotime($_SESSION['created_at'])); ?></small>
                 </p>
               </li>
               
