0c33e0d1770f1cd49b8a180427ad8c62db118f25
Filipe Ferreira <filipeferreira@MacBook-Pro-de-Filipe.local>
2018-12-14 17:22:25 +0000

    add custom js file

--- application/views/footer.php
+++ application/views/footer.php
@@ -11,7 +11,8 @@
         </div>
               
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>      
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>   
+        <script src="<?php echo base_url('assets/js/custom.js');?>"></script>   
     </footer>
 
 </body>
