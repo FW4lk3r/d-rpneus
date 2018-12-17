76a3435a7d5730859097e7484160ccdf257a730b
Filipe Ferreira <filipeferreira@MBP-de-Filipe.lan>
2018-12-17 03:40:01 +0000

    add favicon

--- application/views/header.php
+++ application/views/header.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style-frontend.css') ?>">
+        <link rel="icon" type="image/png" href="<?= base_url('assets/img/favicon.ico');?>" sizes="32x32">
         <META charset="utf-8">
         <title> D&R PNEUS </title>
         </head>
