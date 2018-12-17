05ff95d9677089625f010dd5deb72c6732b521c8
Filipe Ferreira <filipeferreira@MBP-de-Filipe.lan>
2018-12-17 03:39:55 +0000

    add footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,20 +1,66 @@
+<div class="clearfix"></div>
 
-  
-  <footer>
+<footer>
+<div class="main-footer">
         <div class="container">
-            <div class="row col_footer">
-                <div class="col-md-12">
-                        © 2018 Copyright:
-                            <a href="#" class="site_link"> nomesite.com</a>
+            <div class="row">
+                <div class="col-md-4 hidden-xs hidden-sm">
+                    <img src="http://localhost:8888/pneus/assets/img/logo.png" width="300" style="margin-left: -15px" class="margin-bottom-10">
+                    <p>Texto descritivo.</p>
+                </div>
+                <div class="col-sm-12 col-md-8 col-lg-7 ">
+                    <div class="row">
+                     
+                
+                <div class="col-sm-offset-8 col-sm-4">
+                    <h4 class="text-white text-uppercase">Apoio ao cliente</h4>
+                    <ul class="list-unstyled">
+                        <li>
+                            <i class="fa fa-fw fa-phone"></i> (+351) 232 099 999
+                        </li>
+                        <li>
+                            <i class="fa fa-fw fa-mobile bigger-140 padding-left-5 padding-right-5"></i> (+351) 913 701 515
+                        </li>
+                        <li>
+                            <i class="fa fa-fw fa-envelope"></i> geral@asfaltolargo.pt
+                        </li>
+                        <li>
+                            <a href="http://www.facebook.com/asfaltolargo" class="link-animated" target="_blank"><i class="fab fa-fw fa-facebook"></i> Facebook</a>
+                        </li>
+                    </ul>
+                </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <div class="credits">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    ©2018. Asfaltolargo Unipessoal Lda. |
+                    <a href="https://asfaltolargo.pt/avisos-legais">Avisos Legais</a>
+                </div>
+                <div class="col-md-4">
+                    <div class="poweredby pull-right">
+                        <a href="http://www.megatec.pt/" target="_blank" alt="Megatec">
+                            <img src="http://enovo.pt/assets/img/signatures/enovo_white.png" alt="Megatec">
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-              
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    </div>
+  
+
+    
+       
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>   
         <script src="<?php echo base_url('assets/js/custom.js');?>"></script>   
-    </footer>
-
 </body>
 
 </html>
