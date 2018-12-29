f6ea10868ea1655ba4c8bc66dfd4844aba8326c1
Filipe Ferreira <filipesferreira94@gmail.com>
2018-12-29 16:11:57 +0000

    add mor details for home page

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -25,6 +25,8 @@ class Home extends CI_Controller {
 		$dados['diametro'] = $this->Front_Model->getDiametro();
 		$dados['largura'] = $this->Front_Model->getLargura();
 		$dados['pneus'] = $this->Front_Model->getPneus();
+		$dados['jantes'] = $this->Front_Model->getJantes();
+		$dados['veiculos'] = $this->Front_Model->getMarcasVeiculo();
 		$dados['definicoes'] = $this->Front_Model->getDefinicoes();
         $this->load->view('header', $dados);
         $this->load->view('index', $dados);
