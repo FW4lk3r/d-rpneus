ff318a482acdd6bcb54c4c7efa2c3a0bc7258aa3
Filipe Ferreira <filipesferreira94@gmail.com>
2019-01-01 22:25:49 +0000

    Remove comments

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,21 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
 		$this->load->model('Front_Model');
@@ -25,13 +10,13 @@ class Home extends CI_Controller {
 		$dados['diametro'] = $this->Front_Model->getDiametro();
 		$dados['largura'] = $this->Front_Model->getLargura();
 		$dados['pneus'] = $this->Front_Model->getPneus();
+		$dados['menu'] = $this->Front_Model->getDadosMenu();
 		$dados['jantes'] = $this->Front_Model->getJantes();
 		$dados['veiculos'] = $this->Front_Model->getMarcasVeiculo();
 		$dados['definicoes'] = $this->Front_Model->getDefinicoes();
         $this->load->view('header', $dados);
         $this->load->view('index', $dados);
         $this->load->view('footer', $dados);
-    }
-    
+	}
     
 }
