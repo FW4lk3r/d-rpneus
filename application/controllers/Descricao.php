d398e7b2cf158a973795c4f92e8d7903b091892c
Filipe Ferreira <filipesferreira94@gmail.com>
2019-01-02 21:22:06 +0000

    optimize function for see pneu

--- application/controllers/Descricao.php
+++ application/controllers/Descricao.php
@@ -3,35 +3,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Descricao extends CI_Controller {
 
-	public function pneu()
+	public function pneu($id = null)
 	{
         $this->load->model('Front_Model');
-        $largura = htmlspecialchars($this->input->post('largura'));
-        $altura = htmlspecialchars($this->input->post('altura'));
-        $diametro = htmlspecialchars($this->input->post('diametro'));
-    
-
-
+        
         $result = $this->db->query("SELECT * FROM pneus 
         INNER JOIN largura ON pneus.largura = largura.id_largura
         INNER JOIN marcas ON pneus.id_marca = marcas.id_marca
         INNER JOIN altura ON pneus.altura = altura.id_altura
         INNER JOIN diametro ON pneus.diametro = diametro.id_diametro
-        WHERE pneus.largura LIKE '$largura' AND pneus.altura LIKE '$altura' AND pneus.diametro LIKE '$diametro' LIMIT 0,5");
+        WHERE id_pneu = '$id'");
         
         if($result->num_rows() > 0){
             // mostrar todos os pneus iguais
             $dados['info_pneu'] = $result->result();
-        } else{
+
+            $largura = $dados['info_pneu'][0]->id_largura;
+            $altura = $dados['info_pneu'][0]->id_altura;
+            $diametro = $dados['info_pneu'][0]->id_diametro;
+
             // procurar relacionados com altura ou largura ou diametro
-            $result = $this->db->query("SELECT * FROM pneus WHERE largura LIKE '$largura' OR altura LIKE '$altura' OR diametro OR '$diametro' LIMIT 0,5");
-            $dados['info_pneu'] = $result->result();
+            $result = $this->db->query("SELECT * FROM pneus WHERE (largura = $largura OR altura = $altura OR diametro = $diametro) AND id_pneu <> $id LIMIT 0,4");
+            $dados['veja_tambem'] = $result->result();
+
+            $dados['definicoes'] = $this->Front_Model->getDefinicoes();
+            $this->load->view('header', $dados);
+            $this->load->view('descricao', $dados);
+            $this->load->view('footer', $dados);
+        } else{
+            redirect('/');
         }
 
-        $dados['definicoes'] = $this->Front_Model->getDefinicoes();
-        $this->load->view('header', $dados);
-        $this->load->view('descricao', $dados);
-        $this->load->view('footer', $dados);
     }
 
     public function jante()
